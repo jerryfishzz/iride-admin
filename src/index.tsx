095f09ab44be1c9d57b71559c33e64b24bffeaef
Jerry Zhang <jerryfish@163.com>
2022-05-03 13:26:20 +1200

    Restructure the project folder
    chore: restructure the project folder
    
    - Move the original app files to legacy folder
    
    - Rename Paperbase to App to replace the original App files

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import Paperbase from './Paperbase';
 import reportWebVitals from './reportWebVitals';
 
 // New way to create the root defferent from 17 and previous
@@ -10,8 +8,7 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container!); // container! is for typescript
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Paperbase />
+    <App />
   </React.StrictMode>
 );
 
