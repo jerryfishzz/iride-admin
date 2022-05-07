88468a7e8d7c522efe450ca9eff677f17cfc28ee
jerryfishzz <jerryfish@163.com>
2022-05-07 20:32:24 +1200

    Add content on AccordionInput
    feat: add content on AccordionInput as input value
    
    - Use useRef to pass a ref on TextField to pass input value
    
    - Add content prop on AccordionInput to get input value from the parent
    
    - Use useEffect to render the content value to the input

--- src/components/AccordionInput.tsx
+++ src/components/AccordionInput.tsx
@@ -2,15 +2,29 @@ import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material'
+import { useEffect, useRef } from 'react'
 
 interface AccordionInputProps {
   title: string
+  content?: string
 }
 
-export default function AccordionInput({ title }: AccordionInputProps) {
+export default function AccordionInput({
+  title,
+  content = '',
+}: AccordionInputProps) {
+  const textFieldRef = useRef<HTMLInputElement>(null)
+
   const lowecaseTitle = title.toLowerCase()
   const capitalizedTitle =
     lowecaseTitle.charAt(0).toUpperCase() + lowecaseTitle.slice(1)
+
+  useEffect(() => {
+    // This condition is to solve the typescript error 'Object is possibly null'
+    if (textFieldRef && textFieldRef.current) {
+      textFieldRef.current.value = content
+    }
+  }, [content])
 
   return (
     <Accordion>
@@ -27,6 +41,7 @@ export default function AccordionInput({ title }: AccordionInputProps) {
           label={capitalizedTitle}
           variant="standard"
           fullWidth
+          inputRef={textFieldRef}
         />
       </AccordionDetails>
     </Accordion>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -105,7 +105,7 @@ export default function Content() {
         >
           <AccordionInput title="subtitle" />
           <AccordionInput title="fit" />
-          <AccordionInput title="sizing" />
+          <AccordionInput title="sizing" content="men sizing" />
         </Box>
       </Paper>
     </>
